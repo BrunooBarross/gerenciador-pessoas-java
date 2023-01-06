b4ec211349e8dd013e815b4c872e6361c2c847f4
Bruno_Barros <barrosbrunofelipe@gmail.com>
2023-01-06 18:12:14 -0300

    first commit

---
+++ src/test/java/com/gerenciador/pessoas/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.pessoas.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
