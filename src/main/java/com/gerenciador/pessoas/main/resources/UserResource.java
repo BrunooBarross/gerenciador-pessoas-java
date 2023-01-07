aedf7032402d996396f2e8e8299ea0eebddc3eb6
Bruno_Barros <barrosbrunofelipe@gmail.com>
2023-01-07 20:15:28 -0300

    feat: adicionando validações na entidade user com spring validator

--- src/main/java/com/gerenciador/pessoas/main/resources/UserResource.java
+++ src/main/java/com/gerenciador/pessoas/main/resources/UserResource.java
@@ -1,15 +1,22 @@
 package com.gerenciador.pessoas.main.resources;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -42,6 +49,21 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(location).body(obj);
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)	
+	public Map<String, String> handleValidation(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			
+			errors.put(fieldName, errorMessage);
+		});
+		
+		return errors;
 	}
 
 }
