d102c14690df62fed8a14fba631637cbe737910c
Bruno_Barros <barrosbrunofelipe@gmail.com>
2023-01-07 20:14:41 -0300

    feat: adicionando validações na entidade user com spring validator

--- src/main/java/com/gerenciador/pessoas/main/entities/User.java
+++ src/main/java/com/gerenciador/pessoas/main/entities/User.java
@@ -4,11 +4,16 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,7 +22,12 @@ public class User implements Serializable {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@NotBlank(message = "Campo não informado")
+	@Column(nullable = false, length = 45)
 	private String name;
+	@NotNull(message = "Campo não informado - deve serguir o padrão '2001-01-01'")
+	@DateTimeFormat()
+	@Column(nullable = false)
 	private Date birthDate;
 
 	public User() {
--- src/main/java/com/gerenciador/pessoas/main/resources/UserResource.java
+++ src/main/java/com/gerenciador/pessoas/main/resources/UserResource.java
@@ -1,16 +1,22 @@
 package com.gerenciador.pessoas.main.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gerenciador.pessoas.main.entities.User;
 import com.gerenciador.pessoas.main.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -30,4 +36,12 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody @Valid User obj) {
+		obj = service.insert(obj);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(location).body(obj);
+	}
+
 }
